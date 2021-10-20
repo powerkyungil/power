235d12e3a6571246288e2a284e92c8f3f94e005c
admin <admin@DESKTOP-9IIFUDS>
2021-10-20 17:46:50 +0900

    chapter6

---
+++ chapter6/src/com/yedam/java/ch0601/Car.java
@@ -0,0 +1,15 @@
+package com.yedam.java.ch0601;
+
+public class Car {
+    // 1 필드
+	String company = "현대자동차";
+	String model = "그렌져";
+	String color = "검정";
+	int maxSpeed = 350;
+	int speed;
+	
+	// 2 생성자
+	
+	// 3 메소드
+	// 선언되는 순서 1-2-3
+}
